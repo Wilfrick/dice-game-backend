e685fabcc3058aeb826a02e44272ee7ad463cdf3
BroadbentJim <jim.r.broadbent@gmail.com>
2023-10-26 13:19:20 +0100

    Correctly implemented JSON Unpacking

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"golang.org/x/net/websocket"
 )
@@ -12,10 +13,24 @@ type NewGame struct {
 	Players []int
 }
 
+type PlayerMove struct {
+	PlayerID int
+	GameID   int
+	MoveSTR  string
+}
+
+type SimpleObject struct {
+	Name             string
+	Age              int
+	Favourite_Colour string
+}
+
 // Echo the data received on the WebSocket.
 func EchoServer(ws *websocket.Conn) {
 	// fmt.Println("Thinking about responding")
 	// time.Sleep(time.Second * 3)
+
+	// "while" in go is written as "for"
 	for {
 		fmt.Println("Actually responding")
 		fmt.Println("Hello from Jim ")
@@ -46,21 +61,41 @@ func EchoServer(ws *websocket.Conn) {
 	}
 }
 
-type SimpleObject struct {
-	Name             string
-	Age              int
-	Favourite_Colour string
+func BroadcastServer(ws *websocket.Conn) {
+	// Read a JSON object from the websocket
+
+	// Unmarshal that object into a PlayerMove
+
+	// For the future:
+	// Will send the MoveSTR to all other players in the same Game.
+
+	for {
+		fmt.Println("In Broadcast Server")
+		buff := make([]byte, 1024)
+		len_read, err := ws.Read(buff)
+		fmt.Println(buff[:len_read])
+		fmt.Println(string(buff))
+		if err != nil {
+			return
+		}
+		var playerMove PlayerMove
+		err = json.Unmarshal(buff[:len_read], &playerMove)
+		if err != nil {
+			fmt.Println("Couldn't unpack JSON")
+			fmt.Println(err.Error())
+		} else {
+			fmt.Println("Successfully unpacked JSON")
+			fmt.Println(playerMove)
+			// encoded_bytes, _ := json.Marshal(playerMove)
+			// fmt.Println(encoded_bytes)
+		}
+
+		ws.Write(buff[:len_read])
+		fmt.Println("Finished copying")
+	}
 }
 
-// This example demonstrates a trivial echo server.
-func main() {
-	// 	http.Handle("/echo", websocket.Handler(EchoServer))
-	// 	err := http.ListenAndServe(":12345", nil)
-	// 	if err != nil {
-	// 		panic("ListenAndServe: " + err.Error())
-	// 	}
-	// 	fmt.Println("Keep running")
-
+func exampleJSON() {
 	// Take this string and print it out
 	stringRepresentationOfJsonObject := "{\"Name\":\"Alex\",\"Age\": 23,\"Favourite_Colour\": \"Blue\"}"
 	fmt.Println(stringRepresentationOfJsonObject)
@@ -85,4 +120,22 @@ func main() {
 	// Print the reconstructed []bytes (by using string())
 	// TODO
 	fmt.Println(string(byteMarshal))
+}
+
+// This example demonstrates a trivial echo server.
+func main() {
+	// http.Handle("/echo", websocket.Handler(EchoServer))
+	// 	err := http.ListenAndServe(":12345", nil)
+	// 	if err != nil {
+	// 		panic("ListenAndServe: " + err.Error())
+	// 	}
+	// 	fmt.Println("Keep running")
+	exampleJSON()
+	http.Handle("/echo", websocket.Handler(BroadcastServer))
+	err := http.ListenAndServe(":12345", nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
+	fmt.Println("Keep running")
+
 }
